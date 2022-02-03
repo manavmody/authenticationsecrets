de60af6c2ee2b1261f84238def7388af59f94aec
manavmody <manavmody0007@gmail.com>
2022-02-04 01:54:43 +0530

    about md5

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 mongoose.connect("mongodb://localhost/userDB", {useNewUrlParser: true});
 
 app.set("view engine", 'ejs');
@@ -16,8 +16,7 @@ const userSchema = new mongoose.Schema({
     password: String    
 });
 
-console.log(process.env.API_KEY);
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]})
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -46,7 +45,7 @@ app.get("/register", function(req,res){
 app.post("/register" , function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save(function(err){

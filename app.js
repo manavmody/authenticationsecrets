7d1cf0bd9631fac94fe7438a62415d33d2ff426f
manavmody <manavmody0007@gmail.com>
2022-02-07 14:38:45 +0530

    passport and sessions

--- app.js
+++ app.js
@@ -8,19 +8,35 @@ const mongoose = require("mongoose");
 const app = express();
 const md5 = require("md5");
 mongoose.connect("mongodb://localhost/userDB", {useNewUrlParser: true});
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 app.set("view engine", 'ejs');
+
+
+app.use(session({
+    secret: "our secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String    
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.use(bodyParser.urlencoded({
@@ -42,20 +58,29 @@ app.get("/register", function(req,res){
     res.render("register");
 })
 
-app.post("/register" , function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+})
 
-    newUser.save(function(err){
+app.post("/register" , function(req,res){
+
+    User.register({email: req.body.username}, req.body.password , function(err,user){
         if(err){
             console.log(err);
+            res.redirect("/register");
         }else{
-            res.render("secrets")
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets")
+            })
         }
     })
+   
 })
+
 
 
 
